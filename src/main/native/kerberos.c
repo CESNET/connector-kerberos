a88a47b45275904e3f48a8b22e2dd7cf79c851e4
František Dvořák <valtri@civ.zcu.cz>
2016-11-19 12:39:42 +0100

    Testing compile of JNI code

--- src/main/native/kerberos.c
+++ src/main/native/kerberos.c
@@ -1,6 +1,11 @@
 #define CC /*
-gcc -I. -D_GNU_SOURCE -DKRBCONN_TEST -W -Wall -g -O0 $0 -o krbconn_test -lkrb5 -lkadm5clnt_mit
-exit $?
+if test -z "${JAVA_HOME}"; then JAVA_HOME=/usr/lib/jvm/java; fi
+CPPFLAGS="-I. -I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux -D_GNU_SOURCE"
+CFLAGS="-fPIC -W -Wall -g -O2"
+LIBS="-lkrb5 -lkadm5clnt_mit"
+gcc $CPPFLAGS $CFLAGS -DKRBCONN_TEST $0 -o krbconn_test $LIBS || exit $?
+gcc $CPPFLAGS $CFLAGS $0 -o libkerberos-connector.so -shared -Wl,-soname,libkerberos-connector.so $LIBS || exit $?
+exit 0
 */
 #include <getopt.h>
 #include <stdio.h>
@@ -156,29 +161,30 @@ JNIEXPORT void JNICALL Java_cz_zcu_KerberosConnector_krb5_1init(JNIEnv * env , j
 	fid = (*env)->GetFieldID(env, cls, "realm", "Ljava/lang/String");
 	str = (*env)->GetObjectField(env, cls, fid);
 	const char* realm = (*env)->GetStringUTFChars(env, str, 0);
-	config->realm = strdup(realm);
+	conf.realm = strdup(realm);
 	(*env)->ReleaseStringUTFChars(env, str, realm);
 
 	fid = (*env)->GetFieldID(env, cls, "principal", "Ljava/lang/String");
 	str = (*env)->GetObjectField(env, cls, fid);
 	const char* principal = (*env)->GetStringUTFChars(env, str, 0);
-	config->principal = strdup(principal);
+	conf.principal = strdup(principal);
 	(*env)->ReleaseStringUTFChars(env, str, principal);
 
 	fid = (*env)->GetFieldID(env, cls, "password", "Ljava/lang/String");
 	str = (*env)->GetObjectField(env, cls, fid);
 	const char* password = (*env)->GetStringUTFChars(env, str, 0);
-	config->password = strdup(password);
+	conf.password = strdup(password);
 	(*env)->ReleaseStringUTFChars(env, str, password);
 
 	fid = (*env)->GetFieldID(env, cls, "keytab", "Ljava/lang/String");
 	str = (*env)->GetObjectField(env, cls, fid);
 	const char* keytab = (*env)->GetStringUTFChars(env, str, 0);
-	config->keytab = strdup(keytab);
+	conf.keytab = strdup(keytab);
 	(*env)->ReleaseStringUTFChars(env, str, keytab);
 
 	//Initialize context
-	if ((krb5_error_code code = krbconn_init(ctx, &conf)) != 0) {
+	krb5_error_code code;
+	if ((code = krbconn_init(ctx, &conf)) != 0) {
 		err = krbconn_error(ctx, code);
 		printf("%s\n", err);
 		free(err);
@@ -195,7 +201,10 @@ JNIEXPORT void JNICALL Java_cz_zcu_KerberosConnector_krb5_1init(JNIEnv * env , j
 }
 
 
-JNIEXPORT void JNICALL Java_cz_zcu_KerberosConnector_krb5_1destroy(JNIEnv *, jobject) {
+JNIEXPORT void JNICALL Java_cz_zcu_KerberosConnector_krb5_1destroy(JNIEnv *env, jobject this) {
+	jfieldID fid;
+	jclass cls;
+
 	cls = (*env)->GetObjectClass(env, this);
 	fid = (*env)->GetFieldID(env, cls, "contextPointer", "J");
 	krbconn_context_t* ctx = (krbconn_context_t*)(*env)->GetLongField(env, this, fid);
@@ -205,7 +214,7 @@ JNIEXPORT void JNICALL Java_cz_zcu_KerberosConnector_krb5_1destroy(JNIEnv *, job
 }
 
 
-JNIEXPORT void JNICALL Java_cz_zcu_KerberosConnector_krb5_1renew(JNIEnv *, jobject) {
+JNIEXPORT void JNICALL Java_cz_zcu_KerberosConnector_krb5_1renew(JNIEnv *env, jobject this) {
 
 }
 
