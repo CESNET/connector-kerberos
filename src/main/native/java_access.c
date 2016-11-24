d6b0b5f93bf28cd227b2cfe1afb73f9e17c1e6af
Milan Ševčík <majlen@civ.zcu.cz>
2016-11-24 15:03:03 +0100

    Cache Field and Method IDs where easily possible
    Improves performance.

--- src/main/native/java_access.c
+++ src/main/native/java_access.c
@@ -6,7 +6,7 @@
 char* jstring_getter(JNIEnv * env, jobject obj, const char* name) {
 	jclass cls = (*env)->GetObjectClass(env, obj);
 	jmethodID mid = (*env)->GetMethodID(env, cls, name, "()Ljava/lang/String;");
-	if (mid == 0) {
+	if (mid == NULL) {
 		return NULL;
 	}
 
@@ -22,6 +22,8 @@ char* jstring_getter(JNIEnv * env, jobject obj, const char* name) {
 }
 
 char* jguardedstring_getter(JNIEnv * env, jobject obj, const char* name, jclass accessor) {
+	static jmethodID gsMid = NULL;
+
 	jclass cls = (*env)->GetObjectClass(env, obj);
 	jmethodID mid = (*env)->GetMethodID(env, cls, name, "()Lorg/identityconnectors/common/security/GuardedString;");
 	if (mid == 0) {
@@ -29,10 +31,13 @@ char* jguardedstring_getter(JNIEnv * env, jobject obj, const char* name, jclass 
 	}
 
 	jobject guarded = (*env)->CallObjectMethod(env, obj, mid);
-	mid = (*env)->GetStaticMethodID(env, accessor, "getString",
-	                                "(Lorg/identityconnectors/common/security/GuardedString;)Ljava/lang/String;");
 
-	jstring str = (*env)->CallStaticObjectMethod(env, accessor, mid, guarded);
+	if (gsMid == NULL) {
+		gsMid = (*env)->GetStaticMethodID(env, accessor, "getString",
+		                                  "(Lorg/identityconnectors/common/security/GuardedString;)Ljava/lang/String;");
+	}
+
+	jstring str = (*env)->CallStaticObjectMethod(env, accessor, gsMid, guarded);
 	if (str == NULL) {
 		// If password is empty, the method returns NULL
 		return NULL;
@@ -45,14 +50,20 @@ char* jguardedstring_getter(JNIEnv * env, jobject obj, const char* name, jclass 
 }
 
 krbconn_context_t* getContext(JNIEnv* env, jobject this) {
+	static jfieldID fid = NULL;
 	jclass cls = (*env)->GetObjectClass(env, this);
-	jfieldID fid = (*env)->GetFieldID(env, cls, "contextPointer", "J");
+	if (fid == NULL) {
+		fid = (*env)->GetFieldID(env, cls, "contextPointer", "J");
+	}
 	krbconn_context_t* ctx = (krbconn_context_t*)(*env)->GetLongField(env, this, fid);
 	return ctx;
 }
 
 void add_princ_to_array(JNIEnv* env, jobjectArray array, int pos, krbconn_principal_t princ, jclass clazz) {
-	jmethodID mid = (*env)->GetMethodID(env, clazz, "<init>", "(Ljava/lang/String;JJJLjava/lang/String;JILjava/lang/String;)V");
+	static jmethodID mid = NULL;
+	if (mid == NULL) {
+		mid = (*env)->GetMethodID(env, clazz, "<init>", "(Ljava/lang/String;JJJLjava/lang/String;JILjava/lang/String;)V");
+	}
 
 	jstring name = (*env)->NewStringUTF(env, princ.name);
 	jstring modifyPrincipal = (*env)->NewStringUTF(env, princ.mod_name);
