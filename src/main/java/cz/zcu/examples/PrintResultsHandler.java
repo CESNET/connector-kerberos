cd6427d233b3659186c4b036c21c2c1b13024e1e
František Dvořák <valtri@civ.zcu.cz>
2017-03-24 20:47:08 +0100

    Update connector example application with new interface

--- src/main/java/cz/zcu/examples/PrintResultsHandler.java
+++ src/main/java/cz/zcu/examples/PrintResultsHandler.java
@@ -1,10 +1,15 @@
 package cz.zcu.examples;
 
 import org.identityconnectors.framework.common.objects.*;
+import org.identityconnectors.framework.spi.SearchResultsHandler;
 
-public class PrintResultsHandler implements ResultsHandler {
+public class PrintResultsHandler implements SearchResultsHandler {
 	public boolean handle(final ConnectorObject connectorObject) {
 		System.out.println(connectorObject.toString());
 		return true;
+	}
+
+	public void handleResult(SearchResult result) {
+		System.out.println("Progress: " + result.getRemainingPagedResults() + "remaining");
 	}
 }
